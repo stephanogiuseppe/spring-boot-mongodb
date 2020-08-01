c2d49645a20f67e4a177e766f7e8457b881c7170
stephanogiuseppe <stephano.grp@gmail.com>
2020-08-01 17:17:28 -0300

    initial commit

---
+++ src/test/java/com/mongocrud/api/MongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongocrud.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
